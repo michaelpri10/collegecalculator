f3b4b06f0829580d1f213c790b5caaa2f3083f01
Gina Brusco <gbrusco@nd.edu>
2021-10-31 13:45:55 -0400

    login and create account: done

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 mysql = MySQL(app)
 
 path = "/cse30246/collegecalculator"
+login_username = ""
 
 # configure db
 db = yaml.load(open('db.yaml'), Loader=yaml.Loader)
@@ -17,41 +18,73 @@ app.config['MYSQL_DB'] = db['mysql_db']
 app.secret_key = "databaseproject"
 
 @app.route(path + "/", methods=['GET', 'POST'])
-def home():
-    return render_template('index.html')
-
-@app.route(path + "/login", methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        # get inputs
-        userDetails = request.form
-        username = userDetails['u']
-        password = userDetails['p']
+	if request.method == 'POST': 
+		# get inputs
+		userDetails = request.form
+		username = userDetails['u']
+		password = userDetails['p']
+		
+		# connect to mysql and run queries
+		cur = mysql.connection.cursor()
+		
+		#check if a username / password exists. if not, make an account	
+		cur.execute("SELECT * from login_information where username = '%s' and password = '%s'" % (username, password))
+		login_exists = cur.fetchall()
 
-        # connect to mysql and run queries
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO login_info(username, password) VALUES(%s, %s)", (username, email))
-        mysql.connection.commit() #save changes in the database
-        cur.close()
-        return 'success'
-    return render_template('login.html')
+		mysql.connection.commit() #save changes in the database
+		cur.close()
+		
+		if len(login_exists) is not 0:
+			login_username = username
+			return redirect(url_for('search'))
+		else:
+			return 'Incorrect username or password'
+	
+	return render_template('login.html')
+
+@app.route(path + "/create_account", methods = ["GET", "POST"])
+def create_account():
+	if request.method == "POST":
+		#get inputs
+		new_user_details = request.form
+		new_username = new_user_details['u']
+		new_password = new_user_details['p']
+		
+		cur = mysql.connection.cursor()
+		
+		#check if username already exists
+		cur.execute("SELECT * from login_information where username = '%s'" % new_username)
+		login_exists = cur.fetchall()
+
+		if len(login_exists) is not 0:
+			return 'Username already in use. Please choose another.'
+			return redirect(url_for('/create_account'))
+		else:
+			#insert into the database
+			cur.execute("INSERT INTO login_information(username, password) VALUES(%s, %s)", (new_username, new_password))
+			mysql.connection.commit()	
+			return redirect(url_for('login'))
+		
+		cur.close()
+
+	return render_template('create_account.html')
 
 @app.route(path + "/search", methods=["GET", "POST"])
 def search():
-    if request.method == "POST":
-        parameters = request.form
-        columns, sql_query = generate_query(parameters)
-        session['columns'] = columns
-        session['results'] = results
-        return redirect(url_for('results'))
-
-    return render_template("search.html")
+	if request.method == "POST":
+		parameters = request.form
+		columns, sql_query = generate_query(parameters)
+		session['columns'] = columns
+		session['results'] = results
+		return redirect(url_for('results'))
+	return render_template("search.html")
 
 @app.route(path + "/results")
 def results():
-    columns = session['columns']
-    results = session['results']
-    return render_template('results', columns=columns, results=results)
+	columns = session['columns']
+	results = session['results']
+	return render_template('results', columns=columns, results=results)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+	app.run(host="0.0.0.0", debug=True)
