444a23c0355ee6d2e4f4b77ffe458024ea164bcc
Mike Prieto <mprieto2@nd.edu>
2021-12-02 17:40:10 -0500

    Add results page after search

--- query_schools.py
+++ query_schools.py
@@ -23,11 +23,18 @@ def generate_query(params):
     ncaa = params.get('ncaa')
 
     # Multiple score by this much for each type
-    location_mult = 5 - int(params.get('location_rank'))
-    academics_mult = 5 - int(params.get('academics_rank'))
-    finance_mult = 5 - int(params.get('finance_rank'))
-    other_mult = 5 - int(params.get('other_rank'))
+    location_mult = 6 - int(params.get('location_rank'))
+    academics_mult = 6 - int(params.get('academics_rank'))
+    finance_mult = 6 - int(params.get('finance_rank'))
+    other_mult = 6 - int(params.get('other_rank'))
 
+    # Initial SQL strings
+    where_str = "WHERE u.university_id = si.university_id AND u.university_id = adm.university_id AND u.university_id = fin.university_id AND u.university_id = di.university_id"
+    group_str = "GROUP BY u.university_id"
+
+    select_str = "SELECT u.university_id, u.name, u.city, u.state, u.website, u.campus_location, u.total_enrollment"
+
+    majors_query = ""
     if majors:
         majors_string = "(" + ",".join(majors) + ")"
         majors_query = f"""
@@ -38,17 +45,17 @@ def generate_query(params):
         ORDER BY majors_count
         ) as maj
         """
+        where_str = f"{where_str} AND u.university_id = maj.university_id"
 
     from_str = f"FROM university as u, school_info as si, admission_stats as adm, financial_stats as fin, diversity_stats as di {majors_query}"
-    where_str = "WHERE u.university_id = si.university_id AND u.university_id = adm.university_id AND u.university_id = fin.university_id AND u.university_id = di.university_id AND u.university_id = maj.university_id"
-    group_str = "GROUP BY u.university_id"
 
-    select_str = "SELECT u.university_id, u.name, u.city, u.state, u.website, u.campus_location, u.total_enrollment"
 
     # State selection
     location_order = []
-    if state_location == 1:
-        location_order.append(f"(SUM(u.state = '{state}') * 2)")
+    if state_location == 0:
+        location_order.append(f"(SUM(u.state = '{state}'))")
+    elif state_location == 1:
+        location_order.append(f"(SUM(u.state = '{state}') * 3)")
     elif state_location == 2:
         location_order.append(f"SUM(u.state = '{state}')")
 
@@ -61,23 +68,27 @@ def generate_query(params):
         upper_bound = ENROLLMENT_BOUNDS[int(max(size))][1]
         location_order.append(f"SUM(u.total_enrollment >= {lower_bound})")
         location_order.append(f"SUM(u.total_enrollment <= {upper_bound})")
-    location_str = "((" + " + ".join(location_order) + f") * {location_mult}) as location_order"
+    location_str = "((" + " + ".join(location_order) + f") * {location_mult}) as location_score"
 
     if location_order:
         select_str = f"{select_str}, {location_str}"
+    else:
+        select_str = f"{select_str}, 0 as location_score"
 
     # SAT/ACT Scores
-    score_order = []
+    academics_order = []
     if sat_math > 200:
-        score_order.append(f"SUM(adm.sat_math_25 <= {sat_math})")
+        academics_order.append(f"SUM(adm.sat_math_25 <= {sat_math})")
     if sat_reading > 200:
-        score_order.append(f"SUM(adm.sat_rw_25 <= {sat_reading})")
+        academics_order.append(f"SUM(adm.sat_rw_25 <= {sat_reading})")
     if act > 1:
-        score_order.append(f"SUM(adm.act_25 <= {act})")
-    score_str = "((" + " + ".join(score_order) + f") * {academics_mult}) as score_order"
+        academics_order.append(f"SUM(adm.act_25 <= {act})")
+    academics_str = "(maj.majors_count + (" + " + ".join(academics_order) + f") * {academics_mult}) as academics_score"
 
-    if score_order:
-        select_str = f"{select_str}, {score_str}"
+    if academics_order:
+        select_str = f"{select_str}, {academics_str}"
+    else:
+        select_str = f"{select_str}, 0 as academics_score"
 
     # Financial
     finance_order = []
@@ -87,10 +98,12 @@ def generate_query(params):
         else:
             price_state = "in_state_price" if state_location == 1 else "out_of_state_price"
             finance_order.append(f"SUM(fin.{price_state} <= {tuition})")
-    finance_str = "((" + " + ".join(finance_order) + f") * {finance_mult}) as finance_order"
+    finance_str = "((" + " + ".join(finance_order) + f") * {finance_mult}) as finance_score"
 
     if finance_order:
         select_str = f"{select_str}, {finance_str}"
+    else:
+        select_str = f"{select_str}, 0 as finance_score"
 
     # Other preferences
     other_order = []
@@ -107,22 +120,25 @@ def generate_query(params):
         other_order.append("SUM(si.offers_rotc = 1)")
     if ncaa is not None and int(ncaa):
         other_order.append("SUM(si.ncaa_member = 1)")
-    other_str = "((" + " + ".join(other_order) + f") * {other_mult}) as other_count"
+    other_str = "((" + " + ".join(other_order) + f") * {other_mult}) as other_score"
 
     if other_order:
         select_str = f"{select_str}, {other_str}"
+    else:
+        select_str = f"{select_str}, 0 as other_score"
 
     # Academic priority
     order_str = "ORDER BY"
     ranks = {
-        location_mult: "location_order DESC",
-        academics_mult: "maj.majors_count DESC, score_order DESC, adm.admission_rate ASC",
-        finance_mult: "finance_order DESC, fin.average_price_after_aid ASC",
-        other_mult: "other_count DESC",
+        location_mult: "location_score DESC",
+        academics_mult: "academics_score DESC",
+        finance_mult: "finance_score DESC",
+        other_mult: "other_score DESC",
     }
-    order = [ranks[rank] for rank in range(4, 0, -1)]
-    order_str = "ORDER BY " + ", ".join(order)
+    order = [ranks[rank] for rank in range(5, 1, -1)]
+    order_str = "ORDER BY " + ", ".join(order) + ", u.name ASC"
    
-    limit_str = "LIMIT 20"
+    limit_str = "LIMIT 21"
 
+    columns = "university_id, name, city, state, website, campus_location, total_enrollment"
     return f"{select_str}\n{from_str}\n{where_str}\n{group_str}\n{order_str}\n{limit_str};"
